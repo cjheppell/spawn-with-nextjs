c328307dcfe6868a63fff11d8d63b8f13cd8108a
Chris Heppell <chris.john.heppell@gmail.com>
2020-09-28 13:50:26 +0100

    replace homepage scaffold with example homepage

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,48 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
-export default function Home() {
+function HomePage({ profiles, page, pageCount }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <>
+      <ul>
+        {profiles.map((p) => (
+          <li className="profile" key={p.id}>
+            <Link href={`/profile?id=${p.id}`}>
+              <a>
+                <img src={p.avatar} />
+                <span>{p.name}</span>
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <nav>
+        {page > 1 && (
+          <Link href={`/?page=${page - 1}&limit=9`}>
+            <a>Previous</a>
+          </Link>
+        )}
+        {page < pageCount && (
+          <Link href={`/?page=${page + 1}&limit=9`}>
+            <a className="next">Next</a>
+          </Link>
+        )}
+      </nav>
+    </>
   )
 }
+
+export async function getServerSideProps({ req, query }) {
+  const protocol = req.headers['x-forwarded-proto'] || 'http'
+  const host = req.headers['x-forwarded-host'] || req.headers.host
+  const page = query.page || 1
+  const limit = query.limit || 9
+
+  const res = await fetch(
+    `${protocol}://${host}/api/profiles?page=${page}&limit=${limit}`
+  )
+  const data = await res.json()
+
+  return { props: data }
+}
+
+export default HomePage
