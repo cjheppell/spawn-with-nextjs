681e37b7526ec38d6f7c7a0eafe44cfddb61326e
Chris Heppell <chris.john.heppell@gmail.com>
2020-09-28 15:04:24 +0100

    add sql scripts for creating table and inserting data

---
+++ database/sql/R__001_insert-dev-profiles.sql
@@ -0,0 +1,6 @@
+INSERT INTO profiles (name, email, avatar, address) VALUES 
+  ('Chris Heppell', 'chrisheppell@example.com', 'cjheppell', 'spawn.cc'),
+  ('Santiago Arias', 'santiagoarias@example.com', 'santiaago', 'spawn.cc'),
+  ('Ellie Fuller', 'elliefuller@example.com', 'eFuller96', 'spawn.cc'),
+  ('Matthew Flatt', 'matthewflatt@example.com', 'MatthewFlatt', 'spawn.cc'),
+  ('Andrew Farries', 'andrewfarries@example.com', 'andrew-farries', 'spawn.cc')
---
+++ database/sql/V01__001_create-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE profiles (
+  id SERIAL,
+  name TEXT,
+  email TEXT,
+  avatar TEXT,
+  address TEXT
+)
